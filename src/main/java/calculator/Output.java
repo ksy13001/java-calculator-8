c084697bb90a4bedb95834bd6a1c6aea1e4031e9
DESKTOP-1ROFC7Q\ksy13 <ksy13001@gmail.com>
2025-10-20 22:43:47 +0900

    feat: 출력 기능 구현

---
+++ src/main/java/calculator/Output.java
@@ -0,0 +1,10 @@
+package calculator;
+
+public class Output {
+
+    private static final String PROMPT = "결과 : ";
+
+    static void printResult(int result){
+        System.out.println(PROMPT + result);
+    }
+}
