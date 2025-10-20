49413f265eb0b219dfbf4ffefe9d122d3210ddf7
DESKTOP-1ROFC7Q\ksy13 <ksy13001@gmail.com>
2025-10-20 23:13:33 +0900

    feat: Application 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,13 +1,24 @@
 package calculator;
-import camp.nextstep.edu.missionutils.Console;
+import java.util.List;
+
+import static calculator.DelimiterHeader.PREFIX;
+import static calculator.DelimiterHeader.SUFFIX;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         String input = Input.readInput();
-
-
-
-
+        List<Integer> numbers = DelimiterParser.parseDelimiter(input)
+                .map(s -> {
+                    NumberExtractor numberExtractor = new NumberExtractor(s);
+                    return numberExtractor.extractNumbers(InputParser.extractPayload(input));
+                })
+                .orElseGet(()->{
+                    NumberExtractor numberExtractor = new NumberExtractor();
+                    return numberExtractor.extractNumbers(input);
+                });
+        Output.printResult(Calculator.sum(numbers));
     }
+
+
 }
