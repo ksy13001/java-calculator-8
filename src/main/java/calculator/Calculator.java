bc980ac5abcbe10e919d6b226e28cf908d9f0a37
DESKTOP-1ROFC7Q\ksy13 <ksy13001@gmail.com>
2025-10-20 21:44:21 +0900

    feat: 합산 기능 구현

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,6 +1,22 @@
 package calculator;
 
-import static camp.nextstep.edu.missionutils.Console.readLine;
+import java.util.List;
+
 
 public class Calculator {
+
+    private Calculator(){
+    }
+
+    public static int sum(List<Integer> numbers) {
+        int sum = 0;
+        for (Integer number : numbers) {
+            try {
+                sum = Math.addExact(sum, number);
+            } catch (ArithmeticException e) {
+                throw new IllegalArgumentException("합산 결과가 int 범위를 초과했습니다.");
+            }
+        }
+        return sum;
+    }
 }
